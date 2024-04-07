0519757c3c6e98feb773c93abbea63549088aec1
Bog <bogsacha@gmail.com>
2024-04-08 01:57:42 +0200

    Block queries.

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,5 @@
 const SITES_CHROME_STORAGE_ID = "sites_chrome_storage_id";
+const QUERIES_CHROME_STORAGE_ID = "queries_chrome_storage_id";
 
 const getForbiddenDomains = async (): Promise<string[]> => {
   return new Promise((resolve, reject) => {
@@ -8,9 +9,27 @@ const getForbiddenDomains = async (): Promise<string[]> => {
       } else {
         // Make sure we have a string before calling split
         const urls = result[SITES_CHROME_STORAGE_ID]
-          ? result[SITES_CHROME_STORAGE_ID].split("\n")
+          ? result[SITES_CHROME_STORAGE_ID].toLowerCase().split("\n")
           : [];
         resolve(urls);
+      }
+    });
+  });
+};
+
+const getForbiddenQueries = async (): Promise<Set<string>> => {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get(QUERIES_CHROME_STORAGE_ID, function (result) {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      } else {
+        // Make sure we have a string before calling split
+        const queries: Set<string> = new Set(
+          result[QUERIES_CHROME_STORAGE_ID]
+            ? result[QUERIES_CHROME_STORAGE_ID].toLowerCase().split("\n")
+            : []
+        );
+        resolve(queries);
       }
     });
   });
@@ -44,19 +63,28 @@ const setupBlockingRules = async () => {
 setupBlockingRules();
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (
-    changeInfo.status === "complete" &&
-    tab.url &&
-    tab.url.includes("https://www.bing.com/search")
-  ) {
-    const url = new URL(tab.url);
-    const queryParams = url.search; // Extract query parameters, including the '?'
+  if (changeInfo.status === "complete" && tab.url) {
+    if (tab.url.includes("https://www.bing.com/search")) {
+      const url = new URL(tab.url);
+      const queryParams = url.search; // Extract query parameters, including the '?'
 
-    // Construct the new URL for Google search with the original query parameters
-    const newUrl = `https://www.google.com/search${queryParams}`;
+      // Construct the new URL for Google search with the original query parameters
+      const newUrl = `https://www.google.com/search${queryParams}`;
 
-    // Redirect by updating the tab's URL to Google's search
-    chrome.tabs.update(tabId, { url: newUrl });
+      // Redirect by updating the tab's URL to Google's search
+      chrome.tabs.update(tabId, { url: newUrl });
+    } else if (tab.url.includes("google.com/search")) {
+      getForbiddenQueries().then((forbiddenQueries) => {
+        const url = new URL(tab.url || "");
+        const urlParams = new URLSearchParams(url.search);
+        const queryParams = (urlParams.get("q") || "").split(" ");
+        for (const queryParam of queryParams) {
+          if (forbiddenQueries.has(queryParam.toLowerCase())) {
+            chrome.tabs.update(tabId, { url: "https://www.google.com/search" });
+          }
+        }
+      });
+    }
   }
 });
 
