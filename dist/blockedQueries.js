61cedf22f4e9a56a725e3b176fb396055098eef2
Bog <bogsacha@gmail.com>
2024-04-08 01:58:41 +0200

    Add /dist temporarily. WIll replace with github action in the future.

---
+++ dist/blockedQueries.js
@@ -0,0 +1,1 @@
+import{P as e}from"./PopUp.js";import"./index.js";const o="queries_chrome_storage_id";new e({target:document.body,props:{title:"Queries",chrome_storage_id:o}});
---
+++ dist/blockedSites.js
@@ -0,0 +1,1 @@
+import{P as o}from"./PopUp.js";import"./index.js";const t="sites_chrome_storage_id";new o({target:document.body,props:{title:"Sites",chrome_storage_id:t}});
