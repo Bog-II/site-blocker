00e6b00c3ff66aa2bedfc868a5d3ea8428396dc3
Bog <bogsacha@gmail.com>
2024-04-11 01:31:38 +0200

    Create main function.

--- src/background.ts
+++ src/background.ts
@@ -57,36 +57,42 @@ const setupBlockingRules = async () => {
   );
 };
 
-setupBlockingRules();
+const main = () => {
+  setupBlockingRules();
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete" && tab.url) {
-    if (tab.url.includes("https://www.bing.com/search")) {
-      const url = new URL(tab.url);
-      const queryParams = url.search; // Extract query parameters, including the '?'
+  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === "complete" && tab.url) {
+      if (tab.url.includes("https://www.bing.com/search")) {
+        const url = new URL(tab.url);
+        const queryParams = url.search; // Extract query parameters, including the '?'
 
-      // Construct the new URL for Google search with the original query parameters
-      const newUrl = `https://www.google.com/search${queryParams}`;
+        // Construct the new URL for Google search with the original query parameters
+        const newUrl = `https://www.google.com/search${queryParams}`;
 
-      // Redirect by updating the tab's URL to Google's search
-      chrome.tabs.update(tabId, { url: newUrl });
-    } else if (tab.url.includes("google.com/search")) {
-      getForbiddenQueries().then((forbiddenQueries) => {
-        const url = new URL(tab.url || "");
-        const urlParams = new URLSearchParams(url.search);
-        const queryParams = (urlParams.get("q") || "").split(" ");
-        for (const queryParam of queryParams) {
-          if (forbiddenQueries.has(queryParam.toLowerCase())) {
-            chrome.tabs.update(tabId, { url: "https://www.google.com/search" });
+        // Redirect by updating the tab's URL to Google's search
+        chrome.tabs.update(tabId, { url: newUrl });
+      } else if (tab.url.includes("google.com/search")) {
+        getForbiddenQueries().then((forbiddenQueries) => {
+          const url = new URL(tab.url || "");
+          const urlParams = new URLSearchParams(url.search);
+          const queryParams = (urlParams.get("q") || "").split(" ");
+          for (const queryParam of queryParams) {
+            if (forbiddenQueries.has(queryParam.toLowerCase())) {
+              chrome.tabs.update(tabId, {
+                url: "https://www.google.com/search",
+              });
+            }
           }
-        }
-      });
+        });
+      }
     }
-  }
-});
+  });
 
-chrome.runtime.onMessage.addListener((request) => {
-  if (request.action === "refreshBlockingRules") {
-    setupBlockingRules();
-  }
-});
+  chrome.runtime.onMessage.addListener((request) => {
+    if (request.action === "refreshBlockingRules") {
+      setupBlockingRules();
+    }
+  });
+};
+
+main();
